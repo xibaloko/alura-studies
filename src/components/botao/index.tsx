0de9579f9e7f05cdaa50aab80bf9de2b8e4eec6f
Ulisses <ulissesmilitao@hotmail.com>
2022-09-07 13:06:51 -0300

    implementando função regressiva

--- src/components/botao/index.tsx
+++ src/components/botao/index.tsx
@@ -4,11 +4,12 @@ import style from "./botao.module.scss";
 class Botao extends React.Component<{
   text: string;
   type?: "button" | "submit" | "reset" | undefined;
+  onClick?: () => void;
 }> {
   render() {
-    const { type = "button" } = this.props;
+    const { type = "button", onClick } = this.props;
     return (
-      <button type={type} className={style.botao}>
+      <button type={type} className={style.botao} onClick={onClick}>
         {this.props.text}
       </button>
     );
--- src/components/cronometro/index.tsx
+++ src/components/cronometro/index.tsx
@@ -6,10 +6,11 @@ import { useEffect, useState } from "react";
 import { tempoParaSegundos } from "../../common/utils/time";
 
 interface Props {
-  selecionado: ITarefa | undefined;
+  selecionado: ITarefa | undefined,
+  finalizarTarefa: () => void
 }
 
-export default function Cronometro({ selecionado }: Props) {
+export default function Cronometro({ selecionado, finalizarTarefa }: Props) {
   const [tempo, setTempo] = useState<number>();
 
   useEffect(() => {
@@ -18,13 +19,23 @@ export default function Cronometro({ selecionado }: Props) {
     }
   }, [selecionado]);
 
+  function regressiva(contador: number = 0){
+    setTimeout(() =>{
+      if(contador > 0){
+        setTempo(contador - 1);
+        return regressiva(contador - 1);
+      }
+      finalizarTarefa();
+    }, 1000);
+  }
+
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
       <div className={style.relogioWrapper}>
         <Relogio tempo={tempo} />
       </div>
-      <Botao text="Começar" />
+      <Botao text="Começar" onClick={() => regressiva(tempo)}/>
     </div>
   );
 }
