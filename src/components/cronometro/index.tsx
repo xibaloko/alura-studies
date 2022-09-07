7540345a4ada21c5f42d7c62aa8a32c72309f406
Ulisses <ulissesmilitao@hotmail.com>
2022-09-07 11:54:39 -0300

    selecionando temporizador

--- src/components/cronometro/index.tsx
+++ src/components/cronometro/index.tsx
@@ -2,7 +2,7 @@ import Botao from "../botao";
 import Relogio from "./relogio";
 import style from "./cronometro.module.scss";
 import { ITarefa } from "../../types/tarefas";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { tempoParaSegundos } from "../../common/utils/time";
 
 interface Props {
@@ -12,16 +12,17 @@ interface Props {
 export default function Cronometro({ selecionado }: Props) {
   const [tempo, setTempo] = useState<number>();
 
-  // if (selecionado?.tempo) {
-  //   setTempo(tempoParaSegundos(selecionado.tempo));
-  // }
-  
+  useEffect(() => {
+    if (selecionado?.tempo) {
+      setTempo(tempoParaSegundos(selecionado.tempo));
+    }
+  }, [selecionado]);
+
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
-      Tempos: {tempo}      
       <div className={style.relogioWrapper}>
-        <Relogio />
+        <Relogio tempo={tempo} />
       </div>
       <Botao text="Começar" />
     </div>
